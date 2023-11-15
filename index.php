4cdfd2d5af2c22ba90bb6c271f120aee6755fe8c
alejandro <alejandrozapata73@gmail.com>
2023-11-15 10:10:11 -0300

    subiendo php clase1

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+
+//hola esto es un comentario sola linea
+echo "<h1>hola mundo PHP</h1>";
+
+echo "<br>";
+
+echo "5" + true;
+
+echo "<br>";
+
+echo "hola proband mi <b>nombre:</b> ";
+
+echo 5.2;
+
+$nombre = "Alejandro";
+$numeroA = 15;
+$numeroB = 15;
+
+echo "<br>";
+
+echo "Probando variables <br>";
+
+echo $nombre;
+
+echo $numeroA + $numeroB;
+
+$sumaValores = $numeroA + $numeroB;
+
+$apellido = "Tedesco";
+
+echo "Esto es el valor Suma: " . $sumaValores;
+
+echo "<br> <h2>Esto es valor Suma</h2> {$sumaValores} ";
+
+if($apellido == "Tedesco"){
+
+    echo "ESto es un if verdadero";
+
+}
+
+// < > >= <= != === 
+
+// &&  || !
+
+// while(){
+
+
+// }
+
+// for(){
+
+// }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<h1>Hola esto es html</h1>
+
+<script>
+
+console.log("hola mundo js")
+
+</script>
+    
+</body>
+</html>
+
+
+
+
+
