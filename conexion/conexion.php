030a86e2b1e20f7eb2fd22d149238231ff1563d4
alejandro <alejandrozapata73@gmail.com>
2023-11-30 15:34:03 -0300

    conexion bd

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,23 +1,62 @@
 <?php
 
+//funciones exclusivas para conexion en php (funciones, POO)
 
-echo "probando la conexion";
+/*
+1-localhost
+2-root (usuario de base datos)
+3-"" (contraseña)
+4-cursosphp2023 (nombre de base de datos)
 
-$conexion =mysqli_connect("localhost","root","","cursosphp2023");
+*/
+
+
+$conexion = mysqli_connect("localhost","root","","cursosphp2023");
+
+echo "Probando Conexion a BD php";
 
 if(mysqli_connect_errno()){
-    echo "error de conexion";
+
+    echo "no se conecto por un error: " . mysqli_connect_errno();    
 }else{
-    echo "se conesto";
+    echo "se conecto de forma correcta";
 }
 
-$consultas = mysqli_query($conexion,"select * from cursos4 ");
+//configurar libreria, traer objeto, trer .config, localhost, contraseña, locals..
 
 
-var_dump($consultas);
+//java.. tener Driver de conexion, version php, version mysl, maven, tomcat..
 
-$listado = mysqli_fetch_assoc($consultas);
 
-var_dump($listado);
+$consulta = mysqli_query($conexion,"SELECT * FROM cursos4 ");
 
-echo $listado['nombre'];
+echo "<pre>";
+var_dump($consulta);
+echo "</pre>";
+
+// $listado =mysqli_fetch_assoc($consulta);
+
+$listadoArray = mysqli_fetch_array($consulta);
+
+echo "<pre>";
+var_dump($listadoArray);
+echo "</pre>";
+
+// echo "<br>";
+// echo $listado['id_curso'];
+// echo $listado['nombre'];
+// echo $listado['costo'];
+
+// foreach($listado as $valor){
+//     echo $valor;
+// }
+
+//2-traer de la base de datos en un listado:
+
+https://getbootstrap.com/docs/5.0/content/tables/
+
+//while
+
+?>
+
+
